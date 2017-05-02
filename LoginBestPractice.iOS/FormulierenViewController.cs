b841147ecae8c384f73cab56fce6233fa67cd628
NielsSchutte <niels-schutte@hotmail.com>
2017-05-02 12:06:43 +0200

    Met gedeeltelijk invulformulier

--- LoginBestPractice.iOS/FormulierenViewController.cs
+++ LoginBestPractice.iOS/FormulierenViewController.cs
@@ -10,7 +10,7 @@ namespace LoginBestPractice.iOS
 	{
 		public FormulierenViewController(IntPtr handle) : base(handle)
 		{
-			
+
 		}
 
 		public override void ViewDidLoad()
@@ -20,21 +20,29 @@ namespace LoginBestPractice.iOS
 			RootObject data = Newtonsoft.Json.JsonConvert.DeserializeObject<RootObject>(DataStorage.forms);
 			int hoogteVanButtons = 20;
 
-			for (int i = 0; i<data.formulieren.Count; i++)
+			for (int i = 0; i < data.formulieren.Count; i++)
 			{
 				hoogteVanButtons += 40;
-				UIButton btn = new UIButton(UIButtonType.RoundedRect);
-				btn.SetTitle(data.formulieren[i].formulier_naam, UIControlState.Normal);
-				btn.Frame = new CoreGraphics.CGRect(0, hoogteVanButtons, this.View.Frame.Size.Width, 50);
-				btn.TouchDown += delegate
-				{
-					UIScrollView scrollView = new UIScrollView();
-					scrollView.ViewForBaselineLayout.Frame = new CoreGraphics.CGRect(0, 200, this.View.Frame.Size.Width, 100);
-                    this.View.AddSubview(scrollView);
-				};
-				this.View.AddSubview(btn);
-			}
 
+				this.View.AddSubview(createButton(data.formulieren[i].formulier_naam, hoogteVanButtons));
+			}
+		}
+
+		public UIButton createButton(string formName, int height)
+		{
+			UIButton btn = new UIButton(UIButtonType.RoundedRect);
+			btn.SetTitle(formName, UIControlState.Normal);
+			btn.Frame = new CoreGraphics.CGRect(0, height, this.View.Frame.Size.Width, 50);
+			btn.TouchDown += delegate
+			{
+				var formulierScherm = new UIView();
+				formulierScherm.Frame = new CoreGraphics.CGRect(0, 0, this.View.Frame.Size.Width, this.View.Frame.Size.Height);
+				formulierScherm.BackgroundColor = UIColor.White;
+				NavigationItem.Title = formName;
+				this.View.AddSubview(formulierScherm);		
+             };
+
+			return btn;
 		}
 	}
 }
--- LoginBestPractice.iOS/FormulierenViewController.designer.cs
+++ LoginBestPractice.iOS/FormulierenViewController.designer.cs
@@ -14,20 +14,8 @@ namespace LoginBestPractice.iOS
     [Register ("FormulierenViewController")]
     partial class FormulierenViewController
     {
-        [Outlet]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        UIKit.UIButton button { get; set; }
-
-        [Action ("button_TouchUpInside:")]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        partial void button_TouchUpInside (UIKit.UIButton sender);
-
         void ReleaseDesignerOutlets ()
         {
-            if (button != null) {
-                button.Dispose ();
-                button = null;
-            }
         }
     }
 }
