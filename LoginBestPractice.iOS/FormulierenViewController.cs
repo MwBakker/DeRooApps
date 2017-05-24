bd00c852716b17e22fb6f87aea3eec71161d7701
NielsSchutte <niels-schutte@hotmail.com>
2017-05-24 09:40:09 +0200

    Nu werkt alles

--- LoginBestPractice.iOS/FormulierenViewController.cs
+++ LoginBestPractice.iOS/FormulierenViewController.cs
@@ -22,7 +22,7 @@ namespace LoginBestPractice.iOS
 			base.ViewDidLoad();
 			DataStorage dataStorage = new DataStorage();
 			dataStorage.refresh();
-			Thread.Sleep(1000);
+			Thread.Sleep(2000);
 			RootObject formData = Newtonsoft.Json.JsonConvert.DeserializeObject<RootObject>(DataStorage.forms);
 			int hoogteVanButtons = 20;
 
