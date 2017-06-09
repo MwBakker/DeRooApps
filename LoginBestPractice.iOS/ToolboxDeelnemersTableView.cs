e2b88f55588bc32ebebb47ecedf96ea5b1a1f3a4
NielsSchutte <niels-schutte@hotmail.com>
2017-06-09 12:56:56 +0200

    Dynamische layout toolboxes

---
+++ LoginBestPractice.iOS/ToolboxDeelnemersTableView.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace LoginBestPractice.iOS
+{
+    public partial class ToolboxDeelnemersTableView : UIView
+    {
+        public ToolboxDeelnemersTableView (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
