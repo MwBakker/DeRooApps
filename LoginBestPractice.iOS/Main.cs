dd5fa68c06d00e87f86412efd0a3ec87260be4ed
MwBakker <mwb@Martijns-Mac.local>
2017-06-23 15:20:01 +0200

    change
    Change

--- LoginBestPractice.iOS/Main.cs
+++ LoginBestPractice.iOS/Main.cs
@@ -12,8 +12,7 @@ namespace LoginBestPractice.iOS
         // This is the main entry point of the application.
         static void Main(string[] args)
         {
-            // if you want to use a different Application Delegate class from "AppDelegate"
-            // you can specify it here.
+            // main
             UIApplication.Main(args, null, "AppDelegate");
         }
     }
