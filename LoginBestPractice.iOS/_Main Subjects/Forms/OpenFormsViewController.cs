915edf7c1343b0922113f2010ee0edcc9c43a6ad
MwBakker <mwb@Martijns-Mac.local>
2017-07-04 09:17:47 +0200

    0 crash occured
    Save

---
+++ LoginBestPractice.iOS/_Main Subjects/Forms/OpenFormsViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace LoginBestPractice.iOS
+{
+    public partial class OpenFormsViewController : UIViewController
+    {
+        public OpenFormsViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
