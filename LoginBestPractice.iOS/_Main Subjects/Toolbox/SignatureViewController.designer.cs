db5a49d29a11043dc1593a90a742baf43a619e58
MwBakker <mwb@Martijns-Mac.local>
2017-07-10 14:04:26 +0200

    State safe

--- LoginBestPractice.iOS/_Main Subjects/Toolbox/SignatureViewController.designer.cs
+++ LoginBestPractice.iOS/_Main Subjects/Toolbox/SignatureViewController.designer.cs
@@ -14,8 +14,16 @@ namespace LoginBestPractice.iOS
     [Register ("SignatureViewController")]
     partial class SignatureViewController
     {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton btn_finishToolbox { get; set; }
+
         void ReleaseDesignerOutlets ()
         {
+            if (btn_finishToolbox != null) {
+                btn_finishToolbox.Dispose ();
+                btn_finishToolbox = null;
+            }
         }
     }
 }
